d7dd07b29dc9b2dbf48c10ac5704965768781355
CiuffarellaP <117938895+CiuffarellaP@users.noreply.github.com>
2022-12-07 19:03:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+bool funzione(char &a){
+    if(a>=65 && a<=90){
+        a=a+32;
+        return true;
+    }
+    else if (a>=97 && a<=122){
+        a=a-32;
+        return true;
+    }else{
+        return false;
+    }
+}
